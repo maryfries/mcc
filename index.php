8ab38c418f2aa4df36bf9cae5c12f1a212e9bb34
Mary <mfries@edc.org>
2016-01-27 16:51:47 -0500

    index page, images, css - not working

---
+++ index.php
@@ -0,0 +1,68 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+        <title>The K-12 Mathematics Curriculum Center</title>
+        <link href="css/main.css" rel="stylesheet" type="text/css" media="screen">
+    </head>
+    <body>
+        <div id="container">
+            <div id="header">
+                <h1>Welcome to the K&ndash;12 Mathematics Curriculum Center</h1>
+            </div>
+            <div id="navigation">
+                <ul>
+                    <li><a href="about.php">About Us</a></li>
+                    <li><a href="pubs.php">Publications</a></li>
+                    <li><a href="research.php">Research</a></li>
+                    <li><a href="resources.php">Other Resources </a></li>
+                    <li><a href="contact.php">Contact Us</a></li>
+                </ul>
+                <div id="search">
+                    <form name="look" method="GET" action="http://google2.edc.org/search">
+                        <input name="q" type="text" value="Search K-12" size="20">
+                        <br/><br/>
+                        <input type="submit" value="Search Site" name="btng">
+                        <input type="hidden" name="site" value="mcc">
+                        <input type="hidden" name="output" value="xml_no_dtd">
+                        <input type="hidden" name="client" value="edc_general">
+                        <input type="hidden" name="proxystylesheet" value="edc_general">
+                    </form>
+                </div>
+            </div>
+            <div id="maincontent">
+                <h1>The K&ndash;12 Mathematics Curriculum Center</h1>
+                <img src="images/3squares.gif" width="100" height="35" class ="bigsquares" alt="Squares" title="Squares">
+                <p><img src="images/homepage.jpg" alt="Adult Learners" width="104" height="84" class="leftphoto">The K&ndash;12 Mathematics Curriculum Center aims to help teachers and administrators make thoughtful, informed decisions about mathematics curriculum and instructional  materials. Our projects explore and analyze how curriculum decisions are  made in K&ndash;12 mathematics programs, provide resources that support good curriculum  selection and implementation, and connect research and practice in mathematics  instruction. Funded by the National Science Foundation since 1997, the center  is based at Education Development Center, Inc. </p>
+            </div> 
+            
+            <div id="news">
+                <h2>News &amp; Highlights</h2>
+                <div id="leftcolumn">
+                    <p><img src="images/pubslogo.gif" alt="Resources icon" width="70" height="55">New! If you are preparing for an upcoming textbook adoption, download our <a href="pubs/mbiblionew.asp">New Annotated Bibliography</a> for articles and resources relevant to the selection process.</p>
+                </div>
+                <div id="middlecolumn">
+                    <p><img src="images/key-issues.gif" width="57" height="70">Adopting new mathematics materials and not sure where to start? Check out our <a href="pubs/mgotolists.asp">&ldquo;go to&rdquo; lists</a> for elementary, middle and high school that provide article annotations of key articles related to the selection and implementation of mathematics materials.</p>
+                </div>
+                <div id="rightcolumn">
+                    <p><img src="images/ideas.gif" width="57" height="70">Learn more about the <a href="research/defaultCI.asp">Mathematics Instructional Materials Study at EDC</a>, a recently funded NSF project studying school districts&rsquo; implementation of elementary mathematics materials.</p>
+                </div>
+            </div>
+            
+            <div id="footer">
+                <hr noshade size="1">
+                <div id="textlinks">
+                    <p><a href="default.asp">Home</a>  |  <a href="about/default.asp">About Us</a>  |  <a href="pubs/default.asp">Publications</a>  |  <a href="research/default.asp">Research</a>  |  <a href="resources/default.asp">Other Resources </a>  |  <a href="contact.asp">Contact Us</a></p>
+                </div>
+                <div id="nsf">
+                    <p>The K&ndash;12 Mathematics Curriculum Center is funded by the <a href="http://www.nsf.gov">National Science Foundation</a> to inform and assist schools and districts as they select and implement Standards-based mathematics curricula.</p>
+                </div>
+                <div id="edc">
+                    <p>Site hosted by<br>
+                    <a href="http://main.edc.org">Education Development Center, Inc.</a> <br>
+                    <a href="http://main.edc.org/info/legal.asp">&copy; 2009  All rights reserved.</a></p>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
